d4ddf4f26775cb2ac3ab3684afd14a6f288f9641
Liam Porat <83852796+lporat465@users.noreply.github.com>
2022-11-29 21:22:33 -0800

    Add files via upload

--- posting.py
+++ posting.py
@@ -3,45 +3,21 @@ from typing import List
 
 
 class Posting:
-    def __init__(self, docid: int, count: int, term):
+    def __init__(self, docid: int, count: int):
         self.docid = docid
         self.count = count
-        self.term = term
-        self.allCount = [count]
-        self.queryList = [term]
     
     def __lt__(self, other: Posting):
         return self.docid < other.docid
 
     def __eq__(self, other):
-        if self.docid == other.getID():
-            self.giveAllCount(other.getCount())
-            self.giveQueryList(other.getTerm())
-            return True
-        return False
-
-        
+        return self.docid == other.getID()
+  
     def getID(self):
         return self.docid
 
-    def getTerm(self):
-        return self.term
-
     def getCount(self):
         return self.count
-
-    def giveAllCount(self, cnt):
-        self.allCount.append(cnt)
-
-    def giveQueryList(self,trm):
-        self.queryList.append(trm)
-
-    def getQueryList(self):
-        return self.queryList
-
-    def getAllCount(self):
-        return self.allCount
-        
         
     def __hash__(self):
          return hash(self.getID())
